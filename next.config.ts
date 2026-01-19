fafa0b87a6215bcd0ee85a4a8a0d1e0e63768882
Sagar Arora <sagararora80@gmail.com>
2026-01-19 19:53:01 +0530

    chore: Configure static export and GitHub Actions deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/food-delivery-black-swan-events",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
